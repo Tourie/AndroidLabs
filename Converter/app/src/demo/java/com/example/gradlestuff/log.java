b829973869d6b420b5e1874f1039b39d1f7759c7
Makar Pundis <makar.pundis@yandex.ru>
2020-12-08 17:31:12 +0300

    1lab

---
+++ Converter/app/src/demo/java/com/example/gradlestuff/log.java
@@ -0,0 +1,5 @@
+package com.example.gradlestuff;
+
+public class log {
+
+}
---
+++ Converter/app/src/main/java/com/example/converter/ConvertFragmentInfo.java
@@ -0,0 +1,24 @@
+package com.example.converter;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ConvertFragmentInfo extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fr_convert_info, container, false);
+    }
+}
